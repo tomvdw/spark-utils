8fd689c507ea8c95636f62fe9034599ca6c52d39
Tom van der Weide <tvanderweide@vistaprint.com>
2015-05-05 11:51:42 +0200

    Add CSV reader class

---
+++ src/main/scala/tom/spark/utils/CsvReader.scala
@@ -0,0 +1,20 @@
+package tom.spark.utils
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.SparkContext
+
+case class CsvReader(rawData: RDD[String], separator: String = ",") {
+
+  private lazy val indexedRawData: RDD[(String, Long)] = rawData.zipWithIndex()
+  
+  def getHeaderString: String = {
+    val firstRow = indexedRawData.filter(_._2 == 0)
+    val rowValue = firstRow.map(_._1).collect()
+    rowValue.apply(0)
+  }
+  
+  def getHeaders: Array[String] = getHeaderString.split(",")
+  
+  def getIndexedDataRows = indexedRawData.filter(_._2 > 0)
+  def getDataRows = getIndexedDataRows.map(_._1)
+}
